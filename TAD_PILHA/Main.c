af4a46b647cdfef6268acebfe61a825ef124d3ff
Raylla Ruthiely <rayllaruthielly707616@gmail.com>
2026-09-09 09:49:47 -0300

    alteraçao no menu

--- TAD_PILHA/Main.c
+++ TAD_PILHA/Main.c
@@ -6,13 +6,7 @@
 
 int main(){
     int valor, opcao;
-
-
     Pilha* p = criar_pilha();
-    push(p, 1);
-    push(p, 2);
-    push(p, 3);
-
     int temp;
     while(pop(p, &temp)){
         printf("Valor desempilhado: %d\n", temp);
@@ -20,14 +14,15 @@ int main(){
 
 
     while(1){
-        printf("\nEscolha uma opção:\n");
+        printf("\nEscolha uma opçao:\n");
         printf("1. Empilhar\n");
         printf("2. Desempilhar\n");
         printf("3. Acessar elemento do topo\n");
         printf("4. Tamanho da pilha\n");
-        printf("5. Verificar se a pilha está cheia\n");
+        printf("5. Verificar se a pilha esta cheia\n");
         printf("6. Sair\n");
         scanf("%d", &opcao);
+        while (getchar() != '\n');
 
         switch(opcao){
             case 1:
@@ -36,14 +31,14 @@ int main(){
                 if(push(p, valor)){
                     printf("Valor empilhado com sucesso.\n");
                 } else {
-                    printf("Pilha cheia. Não é possível empilhar.\n");
+                    printf("Pilha cheia. Nao a possivel empilhar.\n");
                 }
                 break;
             case 2:
                 if(pop(p, &valor)){
                     printf("Valor desempilhado com sucesso.\n");
                 } else {
-                    printf("Pilha vazia. Não é possível desempilhar.\n");
+                    printf("Pilha vazia. Nao e possivel desempilhar.\n");
                 }
                 break;
             case 3:
@@ -60,7 +55,7 @@ int main(){
                 destroy_stack(p);
                 exit(0);
             default:
-                printf("Opção inválida. Tente novamente.\n");
+                printf("Opçao invalida. Tente novamente.\n");
         }
     }
 
